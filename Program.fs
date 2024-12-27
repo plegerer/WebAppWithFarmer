339f30a21be67d8ad2a4050314c02d0be5b15a2f
Paul Legerer <ple@hcm-advice.com>
2024-12-27 16:47:02 +0100

    update to dotnet 8 and falco

--- Program.fs
+++ Program.fs
@@ -1,8 +1,25 @@
-﻿open Saturn
-open Giraffe
+﻿module FreeFinanceApi.Program
 
-let app = application {
-    use_router (text "Hello from Saturn, Giraffe and ASP .NET!")
+open Falco
+open Falco.Routing
+open Falco.HostBuilder
+open Microsoft.AspNetCore.Builder
+
+type Person =
+    { FirstName : string
+      LastName : string }
+
+let mapQueryHandler : HttpHandler =
+    Request.mapQuery (fun q ->
+        let first = q.GetString ("FirstName", "John") // Get value or return default value
+        let last = q.GetString ("LastName", "Doe")
+        { FirstName = first; LastName = last })
+        Response.ofJson
+
+webHost [||] {
+    use_https
+    endpoints [
+        get "/" (Response.ofPlainText "Hello World!")
+        get "/callback/" mapQueryHandler
+    ]
 }
-
-run app
--- deploy.fsx
+++ deploy.fsx
@@ -3,18 +3,17 @@
 open Farmer
 open Farmer.Builders
 
-let publishPath = $"{__SOURCE_DIRECTORY__}/bin/Release/net6.0/publish"
+let publishPath = $"{__SOURCE_DIRECTORY__}/bin/Release/net8.0/publish"
 
 if not (System.IO.Directory.Exists publishPath) then
     failwith "The application has not been published for deployment. Run the following command first: dotnet publish -c Release"
 
 let web =
     webApp {
-        name "ENTER-WEB-APP-NAME-HERE" // must be globally unique! url will be this + .azurewebsites.net
+        name "apitestoervapi-westeurope-01" // must be globally unique! url will be this + .azurewebsites.net
         operating_system Linux
-        sku WebApp.Sku.P3V3
-        runtime_stack Runtime.DotNet60
-        always_on
+        sku WebApp.Sku.F1
+        runtime_stack Runtime.DotNet80
         zip_deploy publishPath
     }
 
